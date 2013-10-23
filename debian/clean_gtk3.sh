aaf9fa2cea047d5597fe90ecbab9b19fe4de2b71
Mateusz Łukasik <mati75@linuxmint.pl>
2013-10-23 09:13:12 +0200

    include previous changes

---
+++ debian/clean_gtk3.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -rf debian/tmp/gtk3/usr/share/icons/
+rm -rf debian/tmp/gtk3/usr/share/locale/
+rm -rf debian/tmp/gtk3/usr/share/mime/
+rm -rf debian/tmp/gtk3/usr/share/spacefm/ui/
+rm -rf debian/tmp/gtk3/usr/share/doc/
+
