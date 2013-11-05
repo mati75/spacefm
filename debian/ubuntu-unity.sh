b7d346d8774499e3465827a408d83c8e9d9028ff
Mateusz Łukasik <mati75@linuxmint.pl>
2013-11-05 21:56:17 +0100

    Fix typo

--- debian/ubuntu-unity.sh
+++ debian/ubuntu-unity.sh
@@ -1,6 +1,6 @@
 #Simple fix Ubuntu Unity issue (https://github.com/IgnorantGuru/spacefm/issues/49) without patch.
 #!/bin/sh
-if [ `lsb_release -is` = 'Debian' ]; then
+if [ `lsb_release -is` = 'Ubuntu' ]; then
 echo "WOW!! Ubuntu detected. Let's fix Unity issue!"
 sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk2/usr/share/applications/spacefm.desktop
 sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk3/usr/share/applications/spacefm.desktop
