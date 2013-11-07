5efb6561e3a5d7ac179ba223b1d368bfc3706b05
Mateusz Łukasik <mati75@linuxmint.pl>
2013-11-07 12:53:22 +0100

    new upstream release

--- debian/ubuntu-unity.sh
+++ debian/ubuntu-unity.sh
@@ -1,8 +1,9 @@
-#Simple fix Ubuntu Unity issue (https://github.com/IgnorantGuru/spacefm/issues/49) without patch.
 #!/bin/sh
-if [ `lsb_release -is` = 'Ubuntu' ]; then
-echo "WOW!! Ubuntu detected. Let's fix Unity issue!"
-sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk2/usr/share/applications/spacefm.desktop
-sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk3/usr/share/applications/spacefm.desktop
-fi
+# Simple fix for Ubuntu Unity issue (https://github.com/IgnorantGuru/spacefm/issues/49)
+# without patch.
 
+if [ `lsb_release -is` = 'Ubuntu' ]; then
+    echo "WOW!! Ubuntu detected. Let's fix Unity issue!"
+    sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk2/usr/share/applications/spacefm.desktop
+    sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk3/usr/share/applications/spacefm.desktop
+fi
