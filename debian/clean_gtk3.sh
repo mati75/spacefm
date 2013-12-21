ee8d6a03582263184514301eb8cac657673559ba
Mateusz Łukasik <mati75@linuxmint.pl>
2013-12-21 16:00:25 +0100

    Prepare backport for wheezy, add hal support

--- debian/clean_gtk3.sh
+++ debian/clean_gtk3.sh
@@ -4,3 +4,8 @@ rm -rf debian/tmp/gtk3/usr/share/locale/
 rm -rf debian/tmp/gtk3/usr/share/mime/
 rm -rf debian/tmp/gtk3/usr/share/spacefm/ui/
 rm -rf debian/tmp/gtk3/usr/share/doc/
+rm -rf debian/tmp/hal/usr/share/icons/
+rm -rf debian/tmp/hal/usr/share/locale/
+rm -rf debian/tmp/hal/usr/share/mime/
+rm -rf debian/tmp/hal/usr/share/spacefm/ui/
+rm -rf debian/tmp/hal/usr/share/doc/
--- debian/ubuntu-unity.sh
+++ debian/ubuntu-unity.sh
@@ -6,4 +6,5 @@ if [ `lsb_release -is` = 'Ubuntu' ]; then
     echo "WOW!! Ubuntu detected. Let's fix Unity issue!"
     sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk2/usr/share/applications/spacefm.desktop
     sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk3/usr/share/applications/spacefm.desktop
+    sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/hal/usr/share/applications/spacefm.desktop
 fi
