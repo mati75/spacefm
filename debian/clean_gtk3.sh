11ffd75d7e44c785a27473112a1f79e44ccf2066
Mateusz Łukasik <mati75@linuxmint.pl>
2014-01-10 17:32:32 +0100

    Remove hal support, it's dropped in Debian

--- debian/clean_gtk3.sh
+++ debian/clean_gtk3.sh
@@ -4,8 +4,3 @@ rm -rf debian/tmp/gtk3/usr/share/locale/
 rm -rf debian/tmp/gtk3/usr/share/mime/
 rm -rf debian/tmp/gtk3/usr/share/spacefm/ui/
 rm -rf debian/tmp/gtk3/usr/share/doc/
-rm -rf debian/tmp/hal/usr/share/icons/
-rm -rf debian/tmp/hal/usr/share/locale/
-rm -rf debian/tmp/hal/usr/share/mime/
-rm -rf debian/tmp/hal/usr/share/spacefm/ui/
-rm -rf debian/tmp/hal/usr/share/doc/
