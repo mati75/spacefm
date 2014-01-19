401adbf45faaa8e2080ba7f7d59f9136e5b395ee
Mateusz Łukasik <mati75@linuxmint.pl>
2014-01-19 14:42:35 +0100

    Add removed debian dir

---
+++ debian/ubuntu-unity.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Simple fix for Ubuntu Unity issue (https://github.com/IgnorantGuru/spacefm/issues/49)
+# without patch.
+echo "WOW!! Ubuntu detected. Let's fix Unity issue!"
+sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk2/usr/share/applications/spacefm.desktop
+sed -i s/"Exec=spacefm %F"/"Exec=env UBUNTU_MENUPROXY= spacefm %F"/g debian/tmp/gtk3/usr/share/applications/spacefm.desktop
