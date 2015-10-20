82dfc39a4c7796dafd876a20e85eb32577e3bfba
Mateusz Łukasik <mati75@linuxmint.pl>
2015-10-20 21:02:31 +0200

    upload to unstable

--- debian/clean_gtk3.sh
+++ debian/clean_gtk3.sh
@@ -4,4 +4,4 @@ rm -rf debian/tmp/gtk3/usr/share/locale/
 rm -rf debian/tmp/gtk3/usr/share/mime/
 rm -rf debian/tmp/gtk3/usr/share/spacefm/ui/
 rm -rf debian/tmp/gtk3/usr/share/doc/
-rm -rf debian/tmp/gtk3/etc/spacefm
+rm -rf debian/tmp/gtk3/etc/spacefm/
